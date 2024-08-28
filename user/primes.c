47e67640207d3c49ab70b6e14e65da2a88ad79b7
pipiyu1037 <pipiyu1011@163.com>
2024-08-28 21:24:11 +0800

    another method

--- user/primes.c
+++ user/primes.c
@@ -13,33 +13,34 @@ main(int argc, char *argv[])
         for(int i = 0; ; ++i){
             pipe(p[i+1]);
             int j = 0;
-            int writed = 0;
+            int prime;
+
+            close(p[i][1]);
+            if(read(p[i][0], &prime, sizeof(prime)) > 0){
+                printf("prime %d\n", prime);
+            }else{
+                close(p[i][0]);
+                close(p[i+1][0]);
+                close(p[i+1][1]);
+                exit(0);
+            }
+
             if(fork() == 0){
-                close(p[i][1]);
+                close(p[i][0]);
+            }else{
                 while(read(p[i][0], buf + j, sizeof(int)) > 0){
-                    if(j == 0){
-                        printf("prime %d\n", buf[j]);
-                    }else if(buf[j] % buf[0] != 0){
+                    if(buf[j] % prime != 0){
                         write(p[i + 1][1], (void *)(buf + j), sizeof(int));
-                        writed = 1;
                     }
                     ++j;
                 }
                 close(p[i][0]);
-            }else{
-                close(p[i][0]);
-                close(p[i][1]);
                 close(p[i+1][0]);
                 close(p[i+1][1]);
+
                 wait(0);
                 exit(0);
-            }
-
-            if(writed == 0){
-                close(p[i+1][0]);
-                close(p[i+1][1]);
-                exit(0);
-            }
+            }            
         }
     }else{
         for(int i = 2; i <= upBound; ++i){
