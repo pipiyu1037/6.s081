b1bb2315cd84f29780f533f9c2ac3e1d16a5e313
pipiyu1037 <pipiyu1011@163.com>
2025-03-04 12:05:49 +0800

    submit lab multithreading

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,7 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
-
+pthread_mutex_t put_lock;
 
 double
 now()
@@ -52,7 +52,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(&put_lock);
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&put_lock);
   }
 
 }
@@ -117,7 +119,7 @@ main(int argc, char *argv[])
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
-
+  pthread_mutex_init(&put_lock, NULL);
   //
   // first the puts
   //
--- user/uthread.c
+++ user/uthread.c
@@ -62,6 +62,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)t, (uint64)next_thread);
   } else
     next_thread = 0;
 }
@@ -76,6 +77,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  *(void**)(t->stack) = (void*)func;
+  *(void**)(t->stack + 8) = (void*)(t->stack+STACK_SIZE);
 }
 
 void 
